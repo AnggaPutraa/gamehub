8ea1839749702d7bad3bcadbde215d5c2002d95b
AnggaPutraa <yananggaa@gmail.com>
2022-07-06 16:35:39 +0800

    fix RenderFlex overflowed error in detail page

--- lib/features/game_detail/presentation/page/game_detail_page.dart
+++ lib/features/game_detail/presentation/page/game_detail_page.dart
@@ -49,105 +49,108 @@ class GameDetailPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                            child: Text(
-                              '${game.title}',
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.575,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Text(
+                                '${game.title}',
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold
+                                ),
                               ),
                             ),
-                          ),
-                          const Divider(
-                            thickness: 1.0,
-                            color: Colors.black26,
-                          ),
-                          const SizedBox(height: 8,),
-                          const Text(
-                            'Description',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                            const Divider(
+                              thickness: 1.0,
+                              color: Colors.black26,
                             ),
-                          ),
-                          Text(
-                            '${game.description}',
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.grey.shade700
+                            const SizedBox(height: 8,),
+                            const Text(
+                              'Description',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 12,),
-                          const Text(
-                            'Worth of',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              '${game.description}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.grey.shade700
+                              ),
                             ),
-                          ),
-                          Text(
-                            '${game.worth}' == 'N/A'
-                            ? 'Not applicable.'
-                            : '${game.worth}',
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.grey.shade700
+                            const SizedBox(height: 12,),
+                            const Text(
+                              'Worth of',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 12,),
-                          const Text(
-                            'Available on',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              '${game.worth}' == 'N/A'
+                              ? 'Not applicable.'
+                              : '${game.worth}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.grey.shade700
+                              ),
                             ),
-                          ),
-                          Text(
-                            '${game.platforms}',
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.grey.shade700
+                            const SizedBox(height: 12,),
+                            const Text(
+                              'Available on',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 12,),
-                          const Text(
-                            'Ends in',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              '${game.platforms}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.grey.shade700
+                              ),
                             ),
-                          ),
-                          Text(
-                            '${game.endDate}' == 'N/A'
-                            ? 'Not applicable.'
-                            : DateFormat.yMMMMEEEEd().format(DateTime.parse('${game.endDate}')),
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.grey.shade700
+                            const SizedBox(height: 12,),
+                            const Text(
+                              'Ends in',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 12,),
-                          const Text(
-                            'How to get it?',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              '${game.endDate}' == 'N/A'
+                              ? 'Not applicable.'
+                              : DateFormat.yMMMMEEEEd().format(DateTime.parse('${game.endDate}')),
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.grey.shade700
+                              ),
                             ),
-                          ),
-                          Text(
-                            '${game.instructions}',
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.grey.shade700
+                            const SizedBox(height: 12,),
+                            const Text(
+                              'How to get it?',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 8,),
-                        ],
+                            Text(
+                              '${game.instructions}',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.grey.shade700
+                              ),
+                            ),
+                            const SizedBox(height: 8,),
+                          ],
+                        ),
                       ),
                     ),
                     const ClaimButton()
@@ -160,6 +163,6 @@ class GameDetailPage extends StatelessWidget {
         ],
       ),
     ),
-    resizeToAvoidBottomInset: false
+    resizeToAvoidBottomInset: true
   );
 }
--- lib/features/game_detail/presentation/widget/claim_button.dart
+++ lib/features/game_detail/presentation/widget/claim_button.dart
@@ -9,38 +9,34 @@ class ClaimButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: () {},
-          child: Padding(
-            padding: const EdgeInsets.only(
-              bottom: 12
-            ),
-            child: SizedBox(
-              width: double.infinity,
-              child: NeumorphismBox(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 4
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Claim Giveaway',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey.shade800
-                      ),
-                    ),
-                  ),
-                )
+    return GestureDetector(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 12
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          child: NeumorphismBox(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 4
               ),
-            ),
+              child: Center(
+                child: Text(
+                  'Claim Giveaway',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade800
+                  ),
+                ),
+              ),
+            )
           ),
         ),
-      ],
+      ),
     );
   }
 }
